8079899c022b09ca59814a504146d660ec41507c
Ubuntu <ubuntu@ip-172-31-16-156.us-west-2.compute.internal>
2024-07-19 06:02:31 +0000

    Shell scripting for variables

---
+++ variable.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This DevOPs Duniya
+
+<<comment 
+ Anything written here is not executed
+
+comment
+
+name="Priya"
+
+echo "Name is $name"
+
+echo "Name is $name, and $(date) "
+
+echo " Enter the name : "
+read username
+echo "You entered the username "
