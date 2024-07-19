2f314b75688e152a5baffa8f3ee00fd037efd613
Ubuntu <ubuntu@ip-172-31-16-156.us-west-2.compute.internal>
2024-07-19 09:11:20 +0000

    functions practise

--- conditions.sh
+++ conditions.sh
@@ -3,15 +3,25 @@
 <<Disclaimer
 This is just for practise fun
 Disclaimer
+ 
+#This is function definition 
 
-read -p "is kiyaansh eating properly: " food
-read -p "kiyaansh's rate of fittness % " active
+function is_healthy() {
+
+read -p "is $1 eating properly: " food
+read -p "$1 rate of fittness % " active
 if [[ $food ==  "veg" ]];
 then
-	echo "kiyaansh is eating healthy"
+	echo "$1 is eating healthy"
 elif [[ $active -ge 100 ]];
 then 
-	echo "Kiyaansh is healthy"
+	echo "$1 is healthy"
 else 
-	echo "kiyaansh is not having healthy"
+	echo "$1 is not having healthy"
 fi
+
+}
+
+#This is function call
+
+is_healthy "Harish"
